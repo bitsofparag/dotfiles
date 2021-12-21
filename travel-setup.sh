5087d78a2fd6fd9683b29caf550e93fda35a5c55
Parag <parag@paragnbt.local>
2021-12-21 15:18:53 +0100

    Refactor dotfiles for stow-ing

--- travel-setup.sh
+++ travel-setup.sh
@@ -1,96 +1,188 @@
 #!/bin/bash
+set -o nounset
+set -o errexit
+set -o pipefail
 
-[ -d "/mnt/mydrive" ] && echo "data disk mounted" || $(echo "Please run 'sudo mount -t ntfs /dev/nvme0n1pX /mnt/mydrive"; exit 1)
-if [ -z "${MYTRAVELAPI_TOKEN+x}" ]; then echo "Please set MYTRAVELAPI_TOKEN=token" && exit 1; else echo "Using token $MYTRAVELAPI_TOKEN"; fi
+export $(egrep -v '^#' .env | xargs)
+export LANG=en_US.UTF-8
 
-echo "===> Set password"
-passwd
+DROPBOX_DOWNLOAD_API="https://content.dropboxapi.com/2/files/download"
+MYTRAVELAPI_TOKEN=${MYTRAVELAPI_TOKEN}
+GITHUB_PERSONAL_TOKEN=${GITHUB_PERSONAL_TOKEN}
+GITLAB_PERSONAL_TOKEN=${GITLAB_PERSONAL_TOKEN}
+WORK_EMAIL=${WORK_EMAIL}
+TIMEZONE=${TIMEZONE:-Europe/Berlin}
 
-echo "===> Setting up workspace"
-cd /mnt/mydrive
-mkdir -p Workspace Workspace/play Workspace/work
-cd Workspace
-echo "Using $MYTRAVEL_API token"
-curl -X POST https://content.dropboxapi.com/2/files/download \
-  --header "Authorization: Bearer $MYTRAVELAPI_TOKEN" \
-  --header "Dropbox-API-Arg: {\"path\": \"/Documents/travel\"}" > ~/.ssh/id_mydrive
-chmod 400  ~/.ssh/id_mydrive
-eval $(ssh-agent)
-ssh-add ~/.ssh/id_mydrive
+function install() {
+    if [[ -n "$(command -v apt-get)" ]]; then
+       sudo apt-get install -y --no-install-recommends "${@}"
+    elif [[ -n "$(command -v apt-get)" ]]; then
+        emerge "${@}"
+    fi
+}
+
+# Taken from https://unix.stackexchange.com/questions/125819/bash-function-decorator
+function decorate() {
+    eval "
+    _inner_$(typeset -f "$1")
+    $1"'() {
+      printf "\n\n"
+      echo >&2 "Running '"$1"'..."
+      echo "=============================="
+      _inner_'"$1"' "$@"
+      local ret=$?
+      echo >&2 "Function '"$1"' returned with exit status $ret"
+      return "$ret"
+    }'
+}
 
 
-echo "===> Downloading git repos"
-[ -d "./_" ] && echo "_ exists" || git clone git@gitlab.com:parag_m/knowledge.git _
-[ -d "./play/dotfiles" ] && echo "dotfiles exists" || git clone git@gitlab.com:parag_m/dotfiles.git play/dotfiles
+function environment() {
+    [ -d "~/Workspace" ] && echo "Workspace is mounted/ready" || $(echo "Please run 'sudo mount -t ntfs /dev/nvme0n1pX $HOME/Workspace"; exit 1)
+    if [ -z "${MYTRAVELAPI_TOKEN+x}" ]; then echo "Please set MYTRAVELAPI_TOKEN=token" && exit 1; else echo "Using token $MYTRAVELAPI_TOKEN"; fi
+}
+decorate environment
 
 
-echo "===> Set up git config"
-ln -s $(pwd)/play/dotfiles/.functions ~/.functions
-cp $(pwd)/play/dotfiles/.gitconfig ~/.gitconfig
-cp $(pwd)/play/dotfiles/.gitconfig-play ~/.gitconfig-play
-cat <<EOF
-[includeIf "gitdir:/mnt/mydrive/Workspace/play/"]
-        path = "~/.gitconfig-play"
-[includeIf "gitdir:/mnt/mydrive/Workspace/_/"]
-        path = "~/.gitconfig-play"
-[includeIf "gitdir:/mnt/mydrive/Workspace/work/"]
-        path = "~/.gitconfig-work"
-EOF
-cat <<'EOS' > ~/.gitconfig-work
+function essentials() {
+    install \
+      build-essential \
+      iputils-ping \
+      curl wget \
+      iptables \
+      psmisc \
+      libpq-dev postgresql-client \
+      python3-pip \
+      zip unzip \
+      openssh-client \
+      git \
+      stow \
+      tzdata
+}
+decorate essentials
+
+
+function locale() {
+    install locales locales-all
+    echo "$LANG UTF-8" | sudo tee /etc/locale.gen \
+      && sudo locale-gen $LANG \
+      && sudo update-locale LANG=$LANG LC_CTYPE=$LANG
+}
+decorate locale
+
+
+function timezone() {
+    sudo ln -fs /usr/share/zoneinfo/${TIMEZONE} /etc/localtime
+    sudo dpkg-reconfigure --frontend noninteractive tzdata
+}
+decorate timezone
+
+
+function ssh() {
+    curl -X POST ${DROPBOX_DOWNLOAD_API} \
+         --header "Authorization: Bearer ${MYTRAVELAPI_TOKEN}" \
+         --header "Dropbox-API-Arg: {\"path\": \"/Documents/travel/id_travel\"}" > ~/.ssh/id_travel
+    chmod 400  ~/.ssh/id_travel
+    eval $(ssh-agent)
+    ssh-add ~/.ssh/id_travel
+}
+decorate ssh
+
+
+function code_workspace() {
+    mkdir -p $1 ${1}/play ${1}/work ${1}/vendor
+    [ -d "${1}/_" ] && echo "_ exists" || git clone https://gitlab-ci-token:${GITLAB_PERSONAL_TOKEN}@gitlab.com/parag_m/knowledge.git ${1}/_
+}
+decorate code_workspace
+
+
+function clone_dotfiles() {
+    [ -d "${1}/dotfiles" ] && echo "dotfiles exists" || git clone https://gitlab-ci-token:${GITLAB_PERSONAL_TOKEN}@gitlab.com/parag_m/dotfiles.git ${1}/dotfiles
+}
+decorate clone_dotfiles
+
+
+function dot_zsh() {
+    dotfiles=${1}
+    install zsh
+    stow -d $dotfiles -vSt ~ zsh
+    bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    echo "source ~/.zshenv.common" >> ~/.zshenv
+    echo "export GITLAB_CLI_TOKEN=" >> ~/.zshenv
+    echo "export GITLAB_USER_PARAG=" >> ~/.zshenv
+    echo "source \$ZSH/oh-my-zsh.sh" >> ~/.zshrc
+    echo "source ~/.zshrc.common" >> ~/.zshrc
+}
+decorate dot_zsh
+
+
+function dot_dircolors() {
+    dotfiles=${1}
+    stow -d $dotfiles -vSt ~ dircolors
+}
+decorate dot_dircolors
+
+
+function dot_gitconfig() {
+    dotfiles=${1}
+    w=${2} # Workspace root
+    stow -d $dotfiles -vSt ~ gitconfig
+    perl -pi -e "s/^\[includeIf \"gitdir:~\/Workspace\/play/\[includeIf \"gitdir:${w}/play/" ~/.gitconfig
+    perl -pi -e "s/^\[includeIf \"gitdir:~\/Workspace\/_/\[includeIf \"gitdir:${w}/_/" ~/.gitconfig
+    perl -pi -e "s/^\[includeIf \"gitdir:~\/Workspace\/work/\[includeIf \"gitdir:${w}/work/" ~/.gitconfig
+
+    cat <<'EOS' > ~/.gitconfig-work
 [user]
     name = Parag M.
-    email = parag.majumdar@nbt.ag
+    email = ${WORK_EMAIL}
 EOS
-ln -s $(pwd)/play/dotfiles/.gitignore_global ~/.gitignore_global
-ln -s $(pwd)/play/dotfiles/.gitmessage ~/.gitmessage
+}
+decorate dot_gitconfig
 
 
-echo "==> Tooling setup"
-sudo apt install -y tldr
-sudo apt install -y python3-pip
-git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
-python3 -m pip install --user cookiecutter
-sudo apt install -y jq
-sudo apt install -y silversearcher-ag
-sudo apt install -y ispell
-python3 -m pip install Pygments
-sudo apt install -y texlive-latex-base
-python3 -m pip install awscli
+function emacs() {
+    install -y emacs-gtk
+}
+decorate emacs
 
 
-echo "==> Hashicorp setup"
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-sudo apt-get update && sudo apt-get install -y terraform nomad vault packer consul
+function spacemacs() {
+    git clone -b develop https://github.com/syl20bnr/spacemacs ~/.emacs.d
+    ln -s $(pwd)/play/dotfiles/.spacemacs ~/.spacemacs
+    rm -rf ~/.emacs.d/private/snippets
+    ln -s $(pwd)/play/dotfiles/snippets ~/.emacs.d/private/snippets
+    setxkbmap -option 'ctrl:nocaps'
+}
+decorate spacemacs
 
+function security() {
+    install keepassxc
+}
+decorate security
 
-echo "===> Install and setup zsh"
-sudo apt install -y zsh
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-ln -s $(pwd)/play/dotfiles/.zshrc.common ~/.zshrc.common
-ln -s $(pwd)/play/dotfiles/.zshenv.common ~/.zshenv.common
-echo "source ~/.zshenv.common" >> ~/.zshenv
-echo "export GITLAB_CLI_TOKEN=" >> ~/.zshenv
-echo "export GITLAB_USER_PARAG=" >> ~/.zshenv
-echo "source \$ZSH/oh-my-zsh.sh" >> ~/.zshrc
-echo "source ~/.zshrc.common" >> ~/.zshrc
+# =====================================
+# Execution starts here
+# =====================================
+essentials
 
+locale
 
-echo "==> Install password manager"
-sudo apt install -y keepassxc
+timezone
 
+WORKSPACE_ROOT=$HOME/Workspace
 
-echo "==> Install and setup emacs"
-sudo apt install -y emacs-gtk
+code_workspace ${WORKSPACE_ROOT}
 
+clone_dotfiles ${WORKSPACE_ROOT}/play
 
-echo "==> Setup spacemacs"
-git clone -b develop https://github.com/syl20bnr/spacemacs ~/.emacs.d
-ln -s $(pwd)/play/dotfiles/.spacemacs ~/.spacemacs
-rm -rf ~/.emacs.d/private/snippets
-ln -s $(pwd)/play/dotfiles/snippets ~/.emacs.d/private/snippets
-setxkbmap -option 'ctrl:nocaps'
+DOTREPO=${WORKSPACE_ROOT}/play/dotfiles
+dot_zsh $DOTREPO
+dot_dircolors $DOTREPO
+dot_gitconfig $DOTREPO $WORKSPACE_ROOT
+
+emacs
+
+spacemacs
 
 echo "==> Fin"
 echo "Run chsh -s /bin/zsh"
